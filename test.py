42229e334c00dc602a63dd6cde7547111ef9dd3b
btt46 <tuanthanhbui46@gmail.com>
2022-04-01 17:21:37 +0900

    refactor

--- test.py
+++ test.py
@@ -44,4 +44,4 @@ src_mask = generate_square_subsequent_mask(35).to(device)
 src_mask = src_mask[:8, :8]
 result = model(torch.tensor(vocab(tokenizer("After Abraham Lincoln won the November 1860 presidential "))).to(device),src_mask)
 output_flat = result.view(-1, ntokens)
-print(output_flat)
+print(len(output_flat), len(output_flat[0]))
