2da2d943da63945efa619300a34c53f004734fb4
btt46 <tuanthanhbui46@gmail.com>
2022-04-01 17:24:10 +0900

    refactor

--- test.py
+++ test.py
@@ -40,7 +40,7 @@ model = TransformerModel(ntokens, emsize, nhead, d_hid, nlayers, dropout).to(dev
 model.load_state_dict(torch.load(PATH))
 model.eval()
 
-src_mask = generate_square_subsequent_mask(35).to(device)
+src_mask = generate_square_subsequent_mask(56).to(device)
 
 result = model(torch.tensor(vocab(tokenizer(text))).to(device),src_mask)
 output_flat = result.view(-1, ntokens)
