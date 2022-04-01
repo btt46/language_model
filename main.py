f3184fc552fd4a8ee54ed450924061c64e581c58
btt46 <tuanthanhbui46@gmail.com>
2022-04-01 14:40:11 +0900

    refactor

--- main.py
+++ main.py
@@ -116,8 +116,7 @@ def train(model):
             ms_per_batch = (time.time() - start_time) * 1000 / log_interval
             cur_loss = total_loss / log_interval
             ppl = math.exp(cur_loss)
-            print('| epoch {3d} | {5d}/{5d} batches | lr {2.2f} | ms/batch {5.2f} | loss {5.2f} | ppl {8.2f}'.format(epoch,
-                    batch, num_batches, lr, ms_per_batch, cur_loss, ppl))
+            print(f'| epoch {epoch:3d} | {batch:5d}/{num_batches:5d} batches | lr {lr:2.2f} | ms/batch {ms_per_batch:5.2f} | loss {cur_loss:5.2f} | ppl {ppl:8.2f}')
             total_loss = 0
             start_time = time.time()
 
@@ -147,9 +146,7 @@ for epoch in range(1, epochs + 1):
     val_ppl = math.exp(val_loss)
     elapsed = time.time() - epoch_start_time
     print('-' * 89)
-    print('| end of epoch {3d} | time: {5.2f}s | valid loss {5.2f} | valid ppl {8.2f}'.format(
-        epoch, elapsed, val_loss, val_ppl
-    ))
+    print(f'| end of epoch {epoch:3d} | time: {elapsed:5.2f}s | valid loss {val_loss:5.2f} | valid ppl {val_ppl:8.2f}')
     print('-' * 89)
 
     if val_loss < best_val_loss:
