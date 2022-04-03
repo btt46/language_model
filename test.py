146eeb6cb0209db4448c6365db757ab5427d1c78
btt46 <tuanthanhbui46@gmail.com>
2022-04-03 15:13:59 +0900

    refactor

--- test.py
+++ test.py
@@ -55,7 +55,7 @@ out = m(out)
 # print(seqs)
 # print(len(out))
 
-predicts = out[3].topk(10)
+predicts = out[3].topk(20)
 # print(predicts[1])
 for i in predicts[1]:
         print(vocab.lookup_token(i))
