7595b5cbcc044fe69bb9b9537f6a7386b666c2a8
btt46 <tuanthanhbui46@gmail.com>
2022-04-03 14:28:19 +0900

    refactor

--- test.py
+++ test.py
@@ -29,7 +29,7 @@ model.load_state_dict(torch.load(PATH))
 # seqs = ["The man went to the store with his dog"]
 # seqs = ["I have watched this [mask] and it was awesome"]
 seqs = [
-                "The man went to the store with his [mask]",
+                "The man went to the store with [mask] dog",
                 "I have watched this [mask] and it was awesome"
         ]
 # input_seq = [torch.tensor(vocab(tokenizer(seq)), dtype=torch.long)]
